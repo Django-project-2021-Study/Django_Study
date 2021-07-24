a965d9803c68bdb5281bce60e6d10b9322fb3297
LeeJongKeun <johnny550@naver.com>
2021-07-24 17:54:20 +0900

    1차 Django 예시

---
+++ myproject/myproject/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+from main.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home, name="home"),
+    path('<str:id>', detail, name="detail"),
+    path('new/', new, name="new"),
+    path('create/', create, name="create"),
+    path('edit/<str:id>', edit, name="edit"),
+    path('update/<str:id>', update, name="update"),	
+    path('delete/<str:id>', delete, name="delete"),
+]
